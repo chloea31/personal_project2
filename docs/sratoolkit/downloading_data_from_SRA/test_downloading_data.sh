841f24eb5d349c0ee65cf68e4e8882e700a69c35
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2026-06-27 17:38:01 +0200

    add new version test downloading data from sra database

--- docs/sratoolkit/downloading_data_from_SRA/test_downloading_data.sh
+++ docs/sratoolkit/downloading_data_from_SRA/test_downloading_data.sh
@@ -20,4 +20,15 @@ fastq-dump --split-files -X 10000 SRR1553607 --outdir SRR1553607_subset
 fastq-dump --help
 
 # Using seqkit stat
+seqkit --help
+seqkit stat SRR1553607_1.fastq
 
+# Using fasterq-dump
+fasterq-dump SRR1553607
+
+# Using prefetch
+mkdir prefetch
+cd prefetch
+prefetch SRR1553607 
+ls -l
+fasterq-dump SRR1553607  
