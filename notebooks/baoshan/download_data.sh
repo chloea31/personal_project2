1ea6e2016ffc35ac67722e76fe8c52bb6c665cce
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2026-06-30 16:49:53 +0200

    add first version of bash script to download SRA data functional

--- notebooks/baoshan/download_data.sh
+++ notebooks/baoshan/download_data.sh
@@ -14,11 +14,22 @@
 # conda activate download_data_viruses
 
 
-###################################
-# Downloading data
-###################################
+###############
+### Variable initialization
+###############
 
-for i in $(cat ./data/baoshan/SRR_Acc_List.txt); 
+WORK_DIR=/mnt/c/Users/chloe/Documents/Bioinformatique/personal_project2
+
+
+###################################
+# Data collection
+###################################
+echo ">downloading the datasets"
+
+sed -i 's/\r$//' $WORK_DIR/data/baoshan/SRR_Acc_List.txt
+for i in $(cat $WORK_DIR/data/baoshan/SRR_Acc_List.txt); 
 do
     echo $i
+    echo "fastq-dump $i"
+    fastq-dump --gzip --split-files --outdir $WORK_DIR/data/baoshan/ "$i"
 done 
