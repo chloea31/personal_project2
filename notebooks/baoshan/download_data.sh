31d40834d208981a25645251b77ad21b1f9929bc
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2026-07-02 00:08:07 +0200

    add prefetch and fasterq to download data and convert them into fastq files respectively

--- notebooks/baoshan/download_data.sh
+++ notebooks/baoshan/download_data.sh
@@ -27,21 +27,22 @@ WORK_DIR=/mnt/c/Users/chloe/Documents/Bioinformatique/personal_project2
 echo ">downloading the datasets"
 
 sed -i 's/\r$//' $WORK_DIR/data/baoshan/SRR_Acc_List.txt
-for i in $(cat $WORK_DIR/data/baoshan/SRR_Acc_List.txt); 
-do
-    echo $i
-    if [ ! -d $WORK_DIR/data/baoshan/$i ]; then
-        mkdir -p "$WORK_DIR/data/baoshan/$i"
-    fi 
-    echo "fastq-dump $i"
-    fastq-dump --gzip \
-        --split-files \
-        --readids \
-        --dumpbase \
-        --clip \
-        --outdir $WORK_DIR/data/baoshan/$i \
-        --log-level 5 "$i"
-done 
+# for i in $(cat $WORK_DIR/data/baoshan/SRR_Acc_List.txt); 
+# do
+#     echo $i
+#     if [ ! -d $WORK_DIR/data/baoshan/$i ]; then
+#         mkdir -p "$WORK_DIR/data/baoshan/$i"
+#     fi 
+#     echo "fastq-dump $i"
+#     fastq-dump --gzip \
+#         --split-files \
+#         --readids \
+#         --dumpbase \
+#         --clip \
+#         --outdir $WORK_DIR/data/baoshan/$i \
+#         --log-level 5 "$i"
+# done 
+
 # According to the article, the data provided in SRA database would be already uploaded skipping technicals, so we might want to
 # not include the --skip-technicals option here.
 
@@ -49,3 +50,22 @@ done
 # Command-line launched without all of the previous arguments:
 # fastq-dump --gzip --split-files --readids <accession>
 
+for i in $(cat $WORK_DIR/data/baoshan/SRR_Acc_List.txt); 
+do
+    echo $i
+    if [ ! -d $WORK_DIR/data/baoshan/prefetch/$i ]; then
+        mkdir -p "$WORK_DIR/data/baoshan/prefetch/$i"
+    fi 
+    echo "prefetch $i"
+    prefetch --progress --output-directory $WORK_DIR/data/baoshan/prefetch/$i $i 
+done 
+
+for i in $(cat $WORK_DIR/data/baoshan/SRR_Acc_List.txt); 
+do
+    echo $i
+    if [ ! -d $WORK_DIR/data/baoshan/prefetch/$i/$i/fasterq ]; then
+        mkdir -p "$WORK_DIR/data/baoshan/prefetch/$i/$i/fasterq"
+    fi 
+    echo "fasterq-dump $i"
+    fasterq-dump $i --split-3 --threads 8 --progress --outdir $WORK_DIR/data/baoshan/prefetch/$i/$i/fasterq
+done 
