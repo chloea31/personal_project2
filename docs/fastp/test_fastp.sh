0b64d84367b9803039f46f14b5034e2115d0a693
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2026-08-04 15:51:08 +0200

    add first version of the script to test fastp

---
+++ docs/fastp/test_fastp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###################################################
+###################################################
+###  Test Data Quality Control for short reads  ###
+###################################################
+###################################################
+
+
+###################################
+# Activating the conda environment
+###################################
+
+# conda activate env_qc_data
+
+
+###############
+### Variable initialization
+###############
+
+WORK_DIR=/mnt/c/Users/chloe/Documents/Bioinformatique/personal_project2
+DATA_DIR=/mnt/c/Users/chloe/Documents/Bioinformatique/personal_project2/docs/fastp
